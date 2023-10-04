3e577a1f813f25c49497ec9807193e1f2193424e
davmoon <haechanmoon@naver.com>
2023-10-04 23:03:19 +0900

    mugunghwa.c 제작 시작. jjuggumi.h에서 mugunhwa 합류

--- HNUADV_PJ1/mugunghwa.c
+++ HNUADV_PJ1/mugunghwa.c
@@ -3,3 +3,24 @@
 #include "keyin.h"
 #include <stdio.h>
 
+#define DIR_UP		0
+#define DIR_DOWN	1
+#define DIR_LEFT	2
+#define DIR_RIGHT	3
+
+void mugunghwa(void) {
+	//m_init();
+	system("cls");
+	display();
+
+	while (1) {
+		key_t key = get_key();
+		if (key == K_QUIT) {
+			break;
+		}
+		else if (key != K_UNDEFINED) {
+
+			//move_manual(key);
+		}
+	}
+}
