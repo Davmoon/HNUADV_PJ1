8c816d159eef7f43a740cb47735df4996670b2b9
davmoon <haechanmoon@naver.com>
2023-10-07 23:45:42 +0900

    '무궁화꽃이피었습니다' 구현 완료 같은 줄 안죽기, pass dialog 미구현

--- HNUADV_PJ1/mugunghwa.c
+++ HNUADV_PJ1/mugunghwa.c
@@ -10,10 +10,11 @@
 
 void m_init(void);
 void yh_print(int, int, int, bool); //영희 생성 함수 x, y, 영희 블록 수, 뒤돌아봄 O(true), X(false)
-void move_m_random(int, int); // 백분율 부분 https://coding-factory.tistory.com/667 참조
+void move_m_random(int); // 백분율 부분 https://coding-factory.tistory.com/667 참조
 void pass_zone(void);
 bool cant_seek_behind(int, int); //뒤에 존재하는 경우 확인 함수. 움직일 경우 자신 기준으로 비교하면 될 듯?
 void yh_no_watch(int* sent_len, int yh_period[]);
+void catch_move(int* sent_len, int);
 
 int px[PLAYER_MAX], py[PLAYER_MAX], period[PLAYER_MAX];  // 각 플레이어 위치, 이동 주기
 
@@ -32,7 +33,7 @@ void m_init(void) {
 			} while (!placable(x, y));
 			px[i] = x;
 			py[i] = y;
-			period[i] = randint(100, 500);
+			//period[i] = randint(100, 500); //교수님께 한 phase 당 주기가 달라져야 하는지 질문해야함.
 
 			// (0 .. n_player-1) 왜 0을 붙여야 하는지 모르겠음; 아마 아스키코드 출력형식 때문일 듯?
 			back_buf[px[i]][py[i]] = '0' + i;
@@ -43,10 +44,9 @@ void m_init(void) {
 }
 
 void yh_print(int x, int y, int num, bool look) {
-	char icon;
+	char icon = '@';
 
-	if (!look) icon = '@';
-	else icon = '*';
+	if (!look) icon = '*';
 
 	for (int i = 0; i < num; i++) {
 		back_buf[x + i][y] = icon;
@@ -63,7 +63,7 @@ bool cant_seek_behind(int row, int col) {
 	return true;
 }
 
-void move_m_random(int pnum, int dir) {
+void move_m_random(int pnum) {
 	int nx, ny;
 	int percent[10] = { 0,0,0,0,0,0,0,1,2,3 }; // 70%, 10%, 10%, 10% 구현
 
@@ -109,34 +109,36 @@ void pass_zone(void) {
 void yh_no_watch(int *sent_len, int yh_period[]) {
 	char sent[] = "무궁화꽃이피었습니다";
 	int len = *sent_len; //너무 길어서 넣음
-	int pm_time = 100; // 느려지거나 빨라지기 위해 더하는 밀리sec
+	int pm_time = 50; // 느려지거나 빨라지기 위해 더하는 밀리sec
 
-	if (len < 10) {
+	if (len <= 9) {
 		yh_print(4, 1, 3, false); // 영희 뒤돌아봄
 		gotoxy(N_ROW + 1, len * 2); // 출력 장소로 이동
 
 		// 겹치는 코드 나중에 가능하면 수정
-		if (len < 5 && tick % yh_period[0] == 0) {
+		if (len < 5 &&  yh_period[2] % yh_period[0] == 0) {
 			printf("%c%c", sent[len * 2], sent[len * 2 + 1]); //한글은 2bit, 따라서 %c%c 사용
 			*sent_len += 1;
 		}
-		else if (len != 10 && len >= 5 && tick % yh_period[1] == 0) {
+		else if (len >= 5 && yh_period[2] % yh_period[1] == 0) {
 			printf("%c%c", sent[len * 2], sent[len * 2 + 1]); //한글은 2bit, 따라서 %c%c 사용
 			*sent_len += 1;
+
+			if (*sent_len == 10) {
+				yh_period[2] = 0;
+			}
 		}
 	}
 	else if (len == 10){
-		
-		yh_period[2] += 10; // 무궁화 출력 이후 카운트 해야 하기 때문
+
 		yh_print(4, 1, 3, true);
+		yh_period[2] += 10; // 무궁화 출력 이후 카운트 해야 하기 때문
 
-		// 3초간 멈춤 테스트 코드
-		if (yh_period[2] % 1000 == 0) {
-			gotoxy(N_ROW + 2, 0);
-			printf("%d초 대기", 4 - (yh_period[2] / 1000));
-		}
+		// 대기시간 카운터 테스트 코드
+		gotoxy(N_ROW + 2, 0);
+		printf("%d 밀리초 대기", yh_period[2]);
 
-		if (yh_period[2] == 3000) {
+		if (yh_period[2] % 3000 == 0) {
 			yh_print(4, 1, 3, true);
 			*sent_len = 0;
 			yh_period[2] = 0;
@@ -149,7 +151,17 @@ void yh_no_watch(int *sent_len, int yh_period[]) {
 				yh_period[0] += pm_time;
 				yh_period[1] -= pm_time;
 			}
-		}
+		}		
+	}
+}
+
+void catch_move(int *sent_len, int i) {
+	int len = *sent_len;
+
+	if (len == 10) {
+		player[i] = false;
+		back_buf[px[i]][py[i]] = ' ';
+		n_alive--;
 	}
 }
 
@@ -159,25 +171,35 @@ void mugunghwa(void) {
 	display();
 	//dialog("\"무궁화 꽃이 피었습니다\"");
 
-	//전역변수에서 지역변수로
-	int yh_period[] = { 600, 600, 0 }; // 무궁화 꽃, 피었습니다, 3초 카운터
+	int yh_period[] = { 500, 500, 0 }; // 무궁화 꽃 t, 피었습니다 t, 3초 카운터 t
 	int sent_len = 0; // 출력된 char sent[] 글자 수 카운트
+	int mv_user_hy_front[] = {0,0,0,0,0,0,0,0,0,1};
 
 	while (1) {
 		yh_no_watch(&sent_len, yh_period);
+		yh_period[2] += 10;
 
 		key_t key = get_key();
 		if (key == K_QUIT) {
 			break;
 		}
 		else if (key != K_UNDEFINED) {
-
 			move_manual(key);
+
+			if (player[0] == true) {
+				catch_move(&sent_len, 0);
+			}
 		}
 
 		for (int i = 1; i < n_player; i++) {
+			period[i] = randint(100, 500);
 			if (player[i] == true && tick % period[i] == 0) {
-				move_m_random(i, 1);
+				if (sent_len <= 9) {
+					move_m_random(i);
+				}
+				else if (sent_len == 10 && mv_user_hy_front[randint(0, 9)] == 1) {
+					catch_move(&sent_len, i);
+				}
 			}
 		}
 		
