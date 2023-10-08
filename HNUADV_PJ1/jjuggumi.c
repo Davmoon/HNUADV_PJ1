5d101f9263b2da1927ba8324736f2f5462216e8f
davmoon <brightsun2001@gmail.com>
2023-10-08 22:15:23 +0900

    J에게 받은 ending() 삽입 아직 작동 미확인

--- HNUADV_PJ1/jjuggumi.c
+++ HNUADV_PJ1/jjuggumi.c
@@ -38,6 +38,43 @@ int jjuggumi_init(void) {
 void intro() {
 
 }
+void ending() {
+    // 1명만 남은 경우: 즉시 엔딩으로 넘어가서 쭈꾸미 게임을 종료
+    int alive_count = 0;
+    int last_alive_player = -1;
+    for (int i = 0; i < n_player; i++) {
+        if (player[i]) {
+            alive_count++;
+            last_alive_player = i;
+        }
+    }
+
+    if (alive_count == 1) {
+        printf("게임이 종료됩니다. 단 한 명만 남았습니다.\n");
+        printf("승자: 플레이어 %d\n", last_alive_player + 1);
+        exit(0);
+    }
+
+    // 모든 (미니)게임에서, 게임 마스터가 게임을 강제로 종료할 수 있음
+    char choice;
+    printf("게임을 종료하려면 'q'를 입력하거나 계속하려면 다른 키를 누르세요: ");
+    scanf_s(" %c", &choice);
+
+    if (choice == 'q') {
+        // 2명 이상이 남은 경우
+        if (alive_count > 1) {
+            printf("우승자를 가리지 못했습니다.\n");
+            printf("살아 남은 플레이어:\n");
+            for (int i = 0; i < n_player; i++) {
+                if (player[i]) {
+                    printf("플레이어 %d\n", i + 1);
+                }
+            }
+            exit(0);
+        }
+    }
+}
+
 
 int main(void) {
 	jjuggumi_init();//인원 입력 함수
@@ -47,6 +84,6 @@ int main(void) {
 	//nightgame();
 	//juldarigi();
 	//jebi();
-	//ending();
+	ending();
 	return 0;
 }
