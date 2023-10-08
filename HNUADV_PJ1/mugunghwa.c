5699ca995dd846b636f20d97ad2d38f232cc7dea
davmoon <haechanmoon@naver.com>
2023-10-08 20:44:48 +0900

    포인터 삭제, yh_period[]는 미삭제

--- HNUADV_PJ1/mugunghwa.c
+++ HNUADV_PJ1/mugunghwa.c
@@ -13,29 +13,28 @@ void yh_print(int, int, int, bool); //영희 생성 함수 x, y, 영희 블록 수, 뒤돌아�
 void move_m_random(int); // 백분율 부분 https://coding-factory.tistory.com/667 참조
 void pass_zone(void);
 bool cant_seek_behind(int, int); //뒤에 존재하는 경우 확인 함수. 움직일 경우 자신 기준으로 비교하면 될 듯?
-void yh_no_watch(int* sent_len, int yh_period[]);
-void catch_move(int* sent_len, int, int yh_period[]);
+void yh_no_watch(int yh_period[]);
+void catch_move(int, int yh_period[]);
 
 int px[PLAYER_MAX], py[PLAYER_MAX], period[PLAYER_MAX]; // 각 플레이어 위치, 이동 주기, 패스 여부
+int len = 0;
 
 void m_init(void) {
 	map_init(11, 35);
 	yh_print(4, 1, 3, true);
-	//player[2] = false; player[5] = false; //죽은 플레이어 test 코드
 
 	int x, y;
 	// 끝 자리에 살아남은 플레이어가 랜덤하게 배치되는 코드
 	for (int i = 0; i < n_player; i++) {
 		if (player[i] == true) {
 			do {
-				x = randint(1, 9); //x와 y의 범위를 잘 못 정해주면 무한루프로 출력오류 발생..
-				y = 33; // 0부터 시작이기 때문
+				x = randint(1, 9);
+				y = 33;
 			} while (!placable(x, y));
 			px[i] = x;
 			py[i] = y;
-			//period[i] = randint(50, 100); //교수님께 한 phase 당 주기가 달라져야 하는지 질문해야함.
+			period[i] = randint(50, 100);
 
-			// (0 .. n_player-1) 왜 0을 붙여야 하는지 모르겠음; 아마 아스키코드 출력형식 때문일 듯?
 			back_buf[px[i]][py[i]] = '0' + i;
 		}
 	}
@@ -105,9 +104,8 @@ void pass_zone(void) {
 	}
 }
 
-void yh_no_watch(int *sent_len, int yh_period[]) {
+void yh_no_watch(int yh_period[]) {
 	char sent[] = "무궁화꽃이피었습니다";
-	int len = *sent_len; //너무 길어서 넣음
 	int pm_time = 20; // 느려지거나 빨라지기 위해 더하는 밀리sec
 
 	if (len <= 9) {
@@ -117,13 +115,13 @@ void yh_no_watch(int *sent_len, int yh_period[]) {
 		// 겹치는 코드 나중에 가능하면 수정
 		if (len < 5 &&  yh_period[2] % yh_period[0] == 0) {
 			printf("%c%c", sent[len * 2], sent[len * 2 + 1]); //한글은 2bit, 따라서 %c%c 사용
-			*sent_len += 1;
+			len += 1;
 		}
 		else if (len >= 5 && yh_period[2] % yh_period[1] == 0) {
 			printf("%c%c", sent[len * 2], sent[len * 2 + 1]); //한글은 2bit, 따라서 %c%c 사용
-			*sent_len += 1;
+			len += 1;
 
-			if (*sent_len == 10) {
+			if (len == 10) {
 				yh_period[2] = 0; //무궁화 출력 타이머 초기화
 			}
 		}
@@ -139,7 +137,7 @@ void yh_no_watch(int *sent_len, int yh_period[]) {
 
 		if (yh_period[2] % 3000 == 0) {
 			yh_print(4, 1, 3, true);
-			*sent_len = 0;
+			len = 0;
 			yh_period[2] = 0; //무궁화 3초 대기 타이머 초기화
 
 			gotoxy(N_ROW + 1, 0); // 무궁화 출력을 깨끗이 비움 (더 좋은 방법이 생각나지 않음...)
@@ -154,8 +152,7 @@ void yh_no_watch(int *sent_len, int yh_period[]) {
 	}
 }
 
-void catch_move(int *sent_len, int i, int yh_period[]) {
-	int len = *sent_len;
+void catch_move(int i, int yh_period[]) {
 	int mv_user_hy_front[] = { 0,0,0,0,0,0,0,0,0,1 }; //10% 확률로 움직여 탈락
 
 	//3초 대기시간인 경우(len == 10) 입력 들어오거나(0) 움직이면(1~9) 잡음
@@ -175,10 +172,9 @@ void mugunghwa(void) {
 	//dialog("\"무궁화 꽃이 피었습니다\"");
 
 	int yh_period[] = { 200, 200, 0, 0 }; // 무궁화 꽃 t, 피었습니다 t, 무궁화 전용 타이머(tick에 따르면 오차생김), 페이즈 패스 체크
-	int sent_len = 0; // 출력된 char sent[] 글자 수 카운트
 
 	while (1) {
-		yh_no_watch(&sent_len, yh_period);
+		yh_no_watch(yh_period);
 		
 		key_t key = get_key();
 		if (key == K_QUIT) {
@@ -186,17 +182,15 @@ void mugunghwa(void) {
 		}
 		else if (key != K_UNDEFINED) {
 			move_manual(key);
-			if (sent_len == 10 && player[0] == true) {
+			if (len == 10 && player[0] == true) {
 				player[0] = false;
 				back_buf[px[0]][py[0]] = ' ';
 				n_alive--;
 			}
 		}
-
 		for (int i = 1; i < n_player; i++) {
-			period[i] = randint(20, 50); // 불규칙적인 플레이어 주기 구현
 			if (tick % period[i] == 0) {
-				if (sent_len <= 9) {
+				if (len <= 9) {
 					move_m_random(i);
 				}
 				else{
